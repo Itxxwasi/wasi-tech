4f6b6e10c265e6bce30672e31b52992401949c4b
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-05-09 09:18:01 +0500

    ♕κιиg-м∂❧Ùpdätê By Øwñér

--- plugins/Lexi.rb
+++ plugins/Lexi.rb
@@ -1,4 +1,4 @@
 6363$
 "dmdhdhdy eyeyet yudd
-y is the best c a good time for you and the way you can do it for you and
- time with you and your family and friends
+u and the way you can do ir you and
+ time with you an and friends
