6bd79544fcca65f63b3cd8717c47ebb60cd43762
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-09-13 09:38:16 +0500

    ֎☠κιиg-м∂⏣⏢

--- commands/misc.js
+++ commands/misc.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
+ * @project_name : KING-MD
+ * @author : naveeddogar <https://github.com/naveeddogar>
+ * @description : King,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -60,7 +60,7 @@ async(Void, citel, text,{ isCreator }) => {
          },
          async(Void, citel, text) => {
 let a = await getBuffer(`https://citel-x.herokuapp.com/attp/${text}`)
- return citel.reply(a,{packname:'Secktor',author:'ATTP'},"sticker") 
+ return citel.reply(a,{packname:'king-md',author:'ATTP'},"sticker") 
          }
      )
  cmd({
