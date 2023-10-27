53f20f8290997c9761df924bf9a7213e4d72fe81
Naveed Dogar <111642055+naveeddogar@users.noreply.github.com>
2023-10-27 11:30:13 +0500

    ☣⟣⟤κιиg-м∂⟥⟢☣

---
+++ config.js
@@ -0,0 +1,61 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+
+
+//-----------------------
+// SESSION ID CONECTING 👇🏻
+//-------------------------
+module.exports = {
+SESSION_ID: '', //Put your session id here
+ALIVE: `HELLO I AM ALIVE`
+}
+/*
+
+ Other Configurations are down 👇🏻
+
+
+*/
+//---------------------------------
+// Only use true/false
+//---------------------------------
+global.autoTyping = false //Auto typing...(true for on, false for off)
+global.autoRecord = false // Auto recording...(true for on, false for off)
+global.autoread = false // Auto Message read(true for on, false for off)
+global.alwaysonline = true // Always Online(true for on, false for off)
+global.statusread = true // Auto status read 🗿🖐🏻(true for on, false for off)
+global.private = true // worktype(true for private, false for public)
+global.autoreact = false // Auto react ( verum myra aah 🤣🙌🏻 )
+global.autodlyt = false // Auto YouTube video downloader(true for on, false for off)
+global.largethumb = "true" // for large thumbnail 
+global.version = "1.5.0"
+//---------------------------------
+// Required variables
+//---------------------------------
+global.prefix = "." // use "" for removing prefix
+global.ownername = "ᴛᴇᴀᴍ ᴇx-ʙᴏᴛ-ᴢ" // Bot Owner name
+global.botname = "x-ʙᴏᴛ-ᴍᴅ" //bot name
+global.thumb = "https://i.imgur.com/IIHEyLI.jpg" // bot image overall
+global.ownernumber = ["917012984396","917594898804","919961857267"] //SODO / OWNER NUMBER
+global.caption = "_*ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ x-ʙᴏᴛ-ᴍᴅ*_" // Caption 
+global.packname = `X-BOT-MD` //sticker pack
+global.author = "TEAM EX-BOT-Z 🗿" //sticker name
+//---------------------------------
+// Owner Mention Audio
+//---------------------------------
+global.ownermention = true // to turn off type false  || turn on type true 👌🏻
+global.mtitle = "X-Bot-Md" // mention tittle
+global.mbody = "TEAM EX BOT-Z" // mention discription 
+global.mthumb = 'https://i.imgur.com/J2x8YMw.jpg' // mention thumbnail 
+global.murl = 'https://instagram.com/aswin_sparky.exe'
+global.mentionaudio = ["https://i.imgur.com/h0mNy8p.mp4","https://i.imgur.com/zNWjhDn.mp4","https://i.imgur.com/Ya3325t.mp4","https://i.imgur.com/k4FE9mA.mp4","https://i.imgur.com/X5WGE9W.mp4","https://i.imgur.com/veoDgRH.mp4","https://i.imgur.com/CZ3eJgw.mp4","https://i.imgur.com/6QFiAoM.mp4","https://i.imgur.com/lAbt9ew.mp4"] //mention audios
+//---------------------------------
+// All in One Url ( you can add any url you want )
+//---------------------------------
+global.url = 'https://chat.whatsapp.com/JjzEUDkJgke1vPGK3GAvG6' // mention url
+//---------------------------------
+// No need of editing ( Edit aaki oombanda 🗿🖐🏻) kuduthal edit aakiyal kolamm aayum myra 🖐🏻🤣
+//---------------------------------
+global.IMGBB_KEY = ["76a050f031972d9f27e329d767dd988f", "deb80cd12ababea1c9b9a8ad6ce3fab2", "78c84c62b32a88e86daf87dd509a657a"]
+global.reactemoji = ['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎','🔥','💦','✂️','🍭'] // auto react emoji
+global.sparky = ['☆','✼','　 ҉ ','❏','🍭','△'] // menu design's
