f09322cf849c7ca2380314275030cda903cb6663
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-12-23 13:28:19 +0500

    WASI TECH

--- commands/Instagram.js
+++ commands/Instagram.js
@@ -1,19 +1,5 @@
 
-/**
-========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
- * @version 1.0.1                                                                                             
- ========================================================
- **/
+
  
 const {cmd , Config , prefix } = require('../lib')
 const fetch = require('node-fetch');
