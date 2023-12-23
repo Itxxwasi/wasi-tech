0aff0e3648ed74d0d2b456be470ae57812d9752e
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-12-23 13:34:11 +0500

    WASI TECH

--- commands/Telegraph.js
+++ commands/Telegraph.js
@@ -1,20 +1,5 @@
 
-/**
-========================================================
-     █▀ █▀▀ █▀▀ ▀█▀ █▀█ █▀█ ▄▄ ▀█ ░ █▀█           |•|
-     ▄█ ██▄ █▄▄ ░█░ █▄█ █▀▄ ░░ █▄ ▄ █▄█           |•|
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : Secktor-2.0                                                                    
- * @author : Slasher-Official <https://github.com/X-S-L-A-S-H-E-R>   
- * @description : Secktor-2.0 ,A Multi-functional whatsapp bot.       
- * @version 2.0.1                                                                                             
- ========================================================
- **/
- 
+
  
 const { cmd }   = require('../lib');
 const util = require('util');
