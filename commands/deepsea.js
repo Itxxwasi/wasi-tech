369f93124079d4674ac2287c5aec03f6955b6024
Itxxwasi <142110420+Itxxwasi@users.noreply.github.com>
2023-12-23 13:48:22 +0500

    WASI TECH

--- commands/deepsea.js
+++ commands/deepsea.js
@@ -1,22 +1,7 @@
-/**
-========================================================
-      █▄▀ █ █▄ █ ▄▀     █▄ ▄█ █▀▄
-      █ █ █ █ ▀█ ▀▄█ ▀▀ █ ▀ █ █▄▀
-========================================================
- Copyright (C) 2022.                                                                                        
- Licensed under the  GPL-3.0 License;                                                      
- You may not use this file except in compliance with the License.    
- It is supplied in the hope that it may be useful                                     
- * @project_name : KING-MD                                                                    
- * @author : naveeddogar <https://github.com/naveeddogar>   
- * @description : KING-MD ,A Multi-functional whatsapp bot.       
- * @version 1.0.1                                                                                             
- ========================================================
- **/
 
  const { tlang,cmd,Config } = require('../lib')
  const maker = require('mumaker')
- let cap = `*_GENERATE BY KING-MD_*`
+ let cap = `*_GENERATE BY WASI-TECH_*`
      //---------------------------------------------------------------------------
  cmd({ pattern: "deepsea", category: "textpro", desc: "Some text to image feature with various styles." }, async(Void, citel, text) => {
          if (!text) return citel.reply('_Need text._')
