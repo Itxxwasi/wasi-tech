e6bbb2f81d2678905ac66e0015d8a9d628b43128
Talha Mehmood <154811653+AlvanoBot@users.noreply.github.com>
2024-01-18 01:25:07 +0400

    Update config.js

--- config.js
+++ config.js
@@ -3,20 +3,20 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER.split(",")
+global.owner = process.env.OWNER_NUMBER.split("971509616598")
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Maher:Zubair@sigma-male.ggwx4gc.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'wasitec@gmail.com'
 global.github = 'https://github.com/itxxwasi/wasi-tech'
 global.location = 'pakistan'
-global.gurl = 'https://instagram.com/chwaseem565_' // add your username
-global.sudo = process.env.SUDO || '923135673658'
+global.gurl = 'https://instagram.com/User-Die_' // add your username
+global.sudo = process.env.SUDO || '971509616598'
 global.devs = '923192173398';
 global.website = 'https://github.com/itxxwasi/wasi-tech' // Hello world
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/0e3ceca721a6e277dbb61.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/c0374d3b9652d86c32828.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'WASI-TECH' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'WASITECH' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𝐢𝐭𝐬_^𝐓𝐚𝐥𝐡𝐚🫶🏻' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𝐢𝐭𝐬_^𝐓𝐚𝐥𝐡𝐚🫶🏻' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'wasi-tech' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'true' ,
